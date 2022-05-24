1a1b592b652db434aaf2054a29d25814b90db546
yelkar-snehal <yelkarsnehal9@gmail.com>
2022-05-24 14:15:26 +0530

    add test case

--- src/Components/Button/__tests__/Button.test.tsx
+++ src/Components/Button/__tests__/Button.test.tsx
@@ -3,21 +3,28 @@ import Button from '../Button';
 
 afterEach(cleanup)
 
-test('renders Button', () => {
+// it ~ test, descibe is not recommended acc to https://create-react-app.dev/docs/running-tests
+
+it('renders Button', () => {
   render(<Button label={'Test'}/>);
 });
 
-test('renders Button correctly', () => {
+it('renders Button correctly', () => {
   render(<Button label={'Test'}/>);
   expect(screen.getByTestId('button')).toHaveTextContent('Test')
 });
+
+it('renders text', () => {
+  render(<Button label={'Text Test'} />)
+  expect(screen.getByText('Text Test')).toBeInTheDocument()
+})
 
 /**
  * notes:
  * press u to update the failed snapshot, or to remove obsolete snapshot
  * useful in code review, to tally the changed snapshot, whether reqd or not
  */
-test('matches snapshot', () => {
+it('matches snapshot', () => {
   const view = render(<Button label={'Snapshot'} />);
   expect(view).toMatchSnapshot();
 });
